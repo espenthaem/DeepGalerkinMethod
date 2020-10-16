36f1f6ad700ad968badd3078d4684b129a139623
espenthaem <e.tierolff@gmail.com>
2020-10-16 10:14:27 +0200

    added plotting functionality, reworked the data loader and structured the script

--- diffusion_data.py
+++ diffusion_data.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # %% Parameters
 
@@ -18,15 +19,6 @@ steps_per_sample = 10  # number of SGD steps to take before re-sampling
 # Sampling parameters
 nSim_interior = 1000
 nSim_terminal = 25
-
-# Plot options
-n_plot = 41  # Points on plot grid for each dimension
-
-# Save options
-saveOutput = False
-saveName = 'diffusion_data'
-saveFigure = False
-figureName = 'diffusion_data.png'
 
 
 def data_loader():
@@ -44,8 +36,8 @@ def data_loader():
         column_name = x_column_names[column_index]
         if '}-' in column_name:
             column_name = column_name.split('-')[0].replace('{', '').replace('}', '').replace('/', ',').split(',')
-            major_index = column_name[0].replace(' ', '')
-            minor_index = column_name[3].replace(' ', '')
+            major_index = int(column_name[0].replace(' ', ''))
+            minor_index = int(column_name[3].replace(' ', '')) - 1
 
             position = float(str(major_index) + '.' + str(minor_index))
 
@@ -67,6 +59,8 @@ def sampler(nSim_interior, nSim_terminal, time, quantity):
     Args:
         nSim_interior: number of space points in the interior of the function's domain to sample
         nSim_terminal: number of space points at terminal time to sample (terminal condition)
+        time: the time axis
+        quantity: pressure, water saturation or oil saturation array
     '''
 
     # Sampler #1: domain interior
@@ -122,6 +116,87 @@ def loss(model, tInt, xInt, quantityInt, tTer, xTer, quantityTer):
     return L1, L3
 
 
+def slice_plots(time, quantity, sess, split=False):
+    # figure options
+    plt.figure(figsize=(12, 10))
+
+    # time values at which to examine density
+    blockPositions = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5]
+
+    for i, blockPosition in enumerate(blockPositions):
+        # specify subplot
+        plt.subplot(5, 2, i + 1)
+
+        t_plot = np.array(time[1:2601])
+        x_plot = blockPosition * np.ones(t_plot.shape[0])
+
+        # simulate process at current t
+        functionValue = quantity[blockPosition][1:2601]
+
+        # prediction
+        fittedValue = sess.run([V], feed_dict={tInt_tnsr: t_plot.reshape(-1, 1), xInt_tnsr: x_plot.reshape(-1, 1)})
+
+        plt.plot(t_plot, functionValue, color='b', label='Analytical Solution', linewidth=3, linestyle=':')
+        plt.plot(t_plot, fittedValue[0], color='r', label='DGM estimate')
+        plt.xlabel('time (days)')
+        plt.ylabel('%s' % quantity_string)
+        plt.title('x = %0.2f' % blockPosition)
+
+        # subplot options
+        # if i == 0:
+        #    plt.legend(loc='upper left', prop={'size': 16})
+
+    # adjust space between subplots
+    plt.subplots_adjust(wspace=0.3, hspace=1.0)
+    if split:
+        plt.savefig('%s_split.png' % quantity_string)
+    else:
+        plt.savefig('%s.png' % quantity_string)
+
+
+# Generate 2D Error plots
+def plots_2d(time, quantity, out_df, split=False):
+    plt.figure(figsize=(12, 10))
+
+    yticklabels = [time.values[0][0], time.values[100][0], time.values[200][0],
+                   time.values[300][0], time.values[400][0], time.values[500][0]]
+    yticklabels = ['%0.1f' % ytickvalue for ytickvalue in yticklabels]
+
+    x_columns = sorted(pressure.columns)
+    ax = sns.heatmap(abs(out_df.values[:500] - quantity[x_columns].values[:500]), cmap="Spectral_r")
+    adjust = ax.set(xlabel='Distance (m)', ylabel='Time',
+                    xticks=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+                    xticklabels=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    yticks=[0, 100, 200, 300, 400, 500], yticklabels=yticklabels,
+                    title='Absolute Error %s saturation' % quantity_string
+                    )
+    ax.invert_yaxis()
+
+    if split:
+        ax.figure.savefig('%s_2D_error_small_split.png' % quantity_string)
+    else:
+        ax.figure.savefig('%s_2D_error_small.png' % quantity_string)
+
+    plt.figure(figsize=(12, 10))
+    yticklabels = [time.values[0][0], time.values[500][0], time.values[1000][0], time.values[1500][0],
+                   time.values[2000][0], time.values[2500][0]]
+    yticklabels = ['%0.1f' % ytickvalue for ytickvalue in yticklabels]
+
+    x_columns = sorted(pressure.columns)
+    ax = sns.heatmap(abs(out_df.values[:2500] - quantity[x_columns].values[:2500]), cmap="Spectral_r")
+    adjust = ax.set(xlabel='Distance (m)', ylabel='Time',
+                    xticks=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+                    xticklabels=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    yticks=[0, 400, 800, 1200, 1600, 2000], yticklabels=yticklabels,
+                    title='Absolute Error %s saturation' % quantity_string
+                    )
+    ax.invert_yaxis()
+
+    if split:
+        ax.figure.savefig('%s_2D_error_split.png' % quantity_string)
+    else:
+        ax.figure.savefig('%s_2D_error.png' % quantity_string)
+
 # %% Set up network
 
 # initialize DGM model (last input: space dimension = 1)
@@ -155,13 +230,16 @@ sess.run(init_op)
 
 # load Data
 time, oil, water, pressure = data_loader()
+pressure = pressure/10000
+quantity = oil
+quantity_string = "oil"
 
 # %% Train network
 # for each sampling stage
 for i in range(sampling_stages):
 
     # sample uniformly from the required regions
-    tInt, xInt, qInt, tTer, xTer, qTer = sampler(nSim_interior, nSim_terminal, time, oil)
+    tInt, xInt, qInt, tTer, xTer, qTer = sampler(nSim_interior, nSim_terminal, time, quantity)
 
     # for a given sample, take the required number of SGD steps
     for _ in range(steps_per_sample):
@@ -171,42 +249,62 @@ for i in range(sampling_stages):
 
     print(loss, L1, L3, i)
 
-# save outout
-if saveOutput:
-    saver = tf.train.Saver()
-    saver.save(sess, './SavedNets/' + saveName)
+# Generate plots
+slice_plots(time, quantity, sess)
 
-# %% Plot results
-# figure options
-plt.figure()
-plt.figure(figsize=(12, 10))
+# Predict function value at every grid point
+x_columns = sorted(pressure.columns)
 
-# time values at which to examine density
-blockPositions = [0.5, 2.5, 4.5, 6.5, 8.5, 9.5]
+out_df = pd.DataFrame(columns=x_columns)
+counter = 0
+for timestep in time.values.reshape(time.values.shape[0]):
+    time_array = timestep * np.ones(len(x_columns))
+    fittedValue = sess.run([V], feed_dict={tInt_tnsr: time_array.reshape(-1, 1), xInt_tnsr: np.array(x_columns).reshape(-1, 1)})
+    out_df.loc[counter] = fittedValue[0].flatten()
+    counter += 1
 
-for i, blockPosition in enumerate(blockPositions):
+# Plot 2D
+plots_2d(time, quantity, out_df)
 
-    # specify subplot
-    plt.subplot(3, 2, i + 1)
+# Split data in train test set, and repeat the process
+sess.close()
+init_op = tf.global_variables_initializer()
 
-    t_plot = np.array(time[1:2001])
-    x_plot = blockPosition * np.ones(t_plot.shape[0])
+# open session
+sess = tf.Session()
+sess.run(init_op)
 
-    # simulate process at current t
-    functionValue = oil[blockPosition][1:2001]
+# Split the data set
+time_split = 20
+train_time = time[time['Time (day)'] < time_split]
+train_q = quantity[time['Time (day)'] < time_split]
 
-    # prediction
-    fittedValue = sess.run([V], feed_dict={tInt_tnsr: t_plot.reshape(-1, 1), xInt_tnsr: x_plot.reshape(-1, 1)})
+# %% Train network
+# for each sampling stage
+for i in range(sampling_stages):
 
-    plt.plot(t_plot, functionValue, color='b', label='Analytical Solution', linewidth=3, linestyle=':')
-    plt.plot(t_plot, fittedValue[0], color='r', label='DGM estimate')
+    # sample uniformly from the required regions
+    tInt, xInt, qInt, tTer, xTer, qTer = sampler(nSim_interior, nSim_terminal, train_time, train_q)
 
-    # subplot options
-    if i == 0:
-        plt.legend(loc='upper left', prop={'size': 16})
+    # for a given sample, take the required number of SGD steps
+    for _ in range(steps_per_sample):
+        loss, L1, L3, _ = sess.run([loss_tnsr, L1_tnsr, L3_tnsr, optimizer],
+                                   feed_dict={tInt_tnsr: tInt, xInt_tnsr: xInt, qInt_tnsr: qInt,
+                                              tTer_tnsr: tTer, xTer_tnsr: xTer, qTer_tnsr: qTer})
 
-# adjust space between subplots
-plt.subplots_adjust(wspace=0.3, hspace=0.4)
+    print(loss, L1, L3, i)
 
-if saveFigure:
-    plt.savefig(figureName)
+# Generate plots
+slice_plots(time, quantity, sess, split=True)
+
+# Predict function value at every grid point
+out_df = pd.DataFrame(columns=x_columns)
+counter = 0
+for timestep in time.values.reshape(time.values.shape[0]):
+    time_array = timestep * np.ones(len(x_columns))
+    fittedValue = sess.run([V], feed_dict={tInt_tnsr: time_array.reshape(-1, 1), xInt_tnsr: np.array(x_columns).reshape(-1, 1)})
+    out_df.loc[counter] = fittedValue[0].flatten()
+    counter += 1
+
+# Plot 2D
+plots_2d(time, quantity, out_df, split=True)
